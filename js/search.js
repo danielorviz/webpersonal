6451037d307cdcd8f183f9b8ed2818a0e6229508
danielorviz <danielorviz@gmail.com>
2023-12-10 19:54:53 +0100

    d

--- js/search.js
+++ js/search.js
@@ -31,16 +31,17 @@ function doSearch(searchTerm) {
     var pageUrls = ["index.html", "aficiones.html", "musica.html", "series.html"]; 
 
     // Realizar búsqueda en cada página
-    pageUrls.forEach(function (url) {
-        fetch(url)
+    var peticiones = pageUrls.map(function (url) {
+        return fetch(url)
             .then(response => response.text())
             .then(html => {
                 var parser = new DOMParser();
                 var doc = parser.parseFromString(html, 'text/html');
 
                 // Busco todos los parrafos del html para buscar palabras
-                var elements = doc.querySelectorAll("p, h1, h2, h3, h4");
-                elements.forEach(function (element, index) {
+                var elementosHtml = doc.querySelectorAll("p, h1, h2, h3, h4");
+                var resultadosBusqueda = [];
+                elementosHtml.forEach(function (element, index) {
                     var content = element.textContent.toLowerCase();
 
                     if (searchTerm.includes(" ")) {
@@ -48,27 +49,42 @@ function doSearch(searchTerm) {
                         if (terminos.every(t => content.includes(t))) {
 
                             var resultItem = element.closest("article, section");
-                            if (!resultsContainer.contains(resultItem)) {
-                                resultsContainer.appendChild(resultItem);
+                            if (resultItem && !resultadosBusqueda.contains(resultItem)) {
+                                resultadosBusqueda.push(resultItem);
                             }
                         }
 
                     } else if (content.includes(searchTerm)) {
                         var resultItem = element.closest("article, section");
-                        resultsContainer.appendChild(resultItem);
+                        if (resultItem && !resultadosBusqueda.contains(resultItem)) {
+                            resultadosBusqueda.push(resultItem);
+                        }
 
                     }
                 });
+                return resultadosBusqueda;
                 
             })
-            .catch(error => console.error("Error al cargar la página", error))
-            .finally(()=>{
-                if (resultsContainer.children.length === 0) {
-                    var noResultsMessage = document.createElement("div");
-                    noResultsMessage.textContent = "No se encontraron resultados.";
-                    resultsContainer.appendChild(noResultsMessage);
-                }
-        });
+            .catch(error => {
+                console.error("Error al cargar la página", error);
+                return [];
+            });
+
+    });
+
+    Promise.all(peticiones)
+        .then(function (resultados){
+            resultados.forEach( function (elemento){
+                resultsContainer.appendChild(elemento);
+            });
+        }).catch(error => console.error("Error al mostrar el resultado", error))
+        .finally(()=>{
+
+            if (resultsContainer.children.length === 0) {
+                var noResultsMessage = document.createElement("p");
+                noResultsMessage.textContent = "No se encontraron resultados.";
+                resultsContainer.appendChild(noResultsMessage);
+            }
     });
     
 
